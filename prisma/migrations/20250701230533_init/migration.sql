3e59f6a1d9ef6627fc2220a6f4fa53dfdadbb836
Gabriel Biankati <gabriel.2023014161@alunocesul.com>
2025-07-01 20:06:39 -0300

    feat: implementado schema.prisma Book

---
+++ prisma/migrations/20250701230533_init/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "books" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "publicationYear" INTEGER NOT NULL,
+    "isbn" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "books_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "books_title_key" ON "books"("title");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "books_isbn_key" ON "books"("isbn");
